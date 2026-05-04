e047b0d2a82abb35bc3b3adbb6f4e38a5ff320e9
Jhon <jfinfocesde@gmail.com>
2026-05-04 15:47:50 -0500

    deploy github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://jhonf.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ScreenHub/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'jhonf', // Usually your GitHub org/user name.
+  projectName: 'ScreenHub', // Usually your repo name.
+  trailingSlash: false,
+
 
   onBrokenLinks: 'throw',
 
